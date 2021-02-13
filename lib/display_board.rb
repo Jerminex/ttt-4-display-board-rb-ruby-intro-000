d5b69d2c259962c83abd89698d8ed0db9cd0581e
remarkable-game-d37c <georgejermine@gmail.com>
2021-02-13 15:10:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,8 @@
 board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  board[0] = "X"
-  puts "#{board[0]}" | "#{board[1]}" | "#{board[2]}"
-  puts "---------"
-  puts "#{board[3]}" | "#{board[4]}" | "#{board[5]}"
-  puts "---------"
-  puts "#{board[6]}" | "#{board[7]}" | "#{board[8]}"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
